ac41f2c8cc75d78f1a12099d267555ad34cac354
Martina Divanisova <martinadivanisova3@gmail.com>
2026-05-22 13:29:25 +0200

    i do not wish to speak

--- src/utils/user.validation.js
+++ src/utils/user.validation.js
@@ -2,7 +2,7 @@ import zod from "zod"
 
 const loginSchema = zod.object({
     username: zod.string().min(5,"Username is too short, must be longer than 5 characters").max(50,"Username is too long, must be less than 20 characters"),
-    password: zod.string().min(7,"Password is too short, must be longer than 7 characters").max(30,"Password is too long, must be less than 30 characters")
+    password: zod.string().min(5,"Password is too short, must be longer than 7 characters").max(30,"Password is too long, must be less than 30 characters")
 });
 
 export {
