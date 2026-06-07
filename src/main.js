1d95a04a4e826e59aa9b07ba5a9701ab5d21aa2f
Martina Divanisova <martinadivanisova3@gmail.com>
2026-06-07 19:11:20 +0200

    fixed CORS error

--- src/main.js
+++ src/main.js
@@ -11,6 +11,18 @@ import attendeeRout from './route/attendees.route.js';
 const app = express();
 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    
+    next();
+});
+
 app.use(express.json());
 
 
